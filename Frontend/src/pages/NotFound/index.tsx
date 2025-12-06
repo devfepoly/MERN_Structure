aba1e59b0ce77d406bcc320b7fca2d921afc8c22
devfepoly <owen.saleonline@gmail.com>
2025-12-06 15:37:40 +0700

    env

--- Frontend/src/pages/NotFound/index.tsx
+++ Frontend/src/pages/NotFound/index.tsx
@@ -12,7 +12,7 @@ const NotFound: FC = memo(() => {
                     to="/" 
                     className="inline-block px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors"
                 >
-                    Back to Home
+                    Back to Home Here
                 </Link>
             </div>
         </div>
